e73053c647684c8d8a8099239a5c1e742d2fafc7
oshiogifted <oshiogifted2011@gmail.com>
2024-09-26 15:02:57 -0400

    modules and export

---
+++ 01-node-tutorial/3-modules.js
@@ -0,0 +1,9 @@
+// CommonJS, every file is module (by default)
+// Modules - Encapsulated Code (only share minimum)
+
+const { john, peter } = require('./4-names');
+const sayHi = require('./5-utils');
+
+sayHi('susan');
+sayHi(john);
+sayHi(peter);
