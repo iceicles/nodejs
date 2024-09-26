3cdf18a4feeed7f61b43710c5f089e9d750d7432
oshiogifted <oshiogifted2011@gmail.com>
2024-09-26 15:15:10 -0400

    module export alternatives

--- 01-node-tutorial/3-modules.js
+++ 01-node-tutorial/3-modules.js
@@ -4,6 +4,10 @@
 const { john, peter } = require('./4-names');
 const sayHi = require('./5-utils');
 
+const data = require('./6-alternative-flavor');
+
+console.log(data);
+
 sayHi('susan');
 sayHi(john);
 sayHi(peter);
