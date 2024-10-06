70f27aab1d37421e1a64def200dbc6e669002f96
oshiogifted <oshiogifted2011@gmail.com>
2024-10-06 18:55:17 -0400

    http html file

--- 2-express-tutorial/app.js
+++ 2-express-tutorial/app.js
@@ -1,6 +1,10 @@
 // setting up server with http module to see why we would want to use express
 
 const http = require('http');
+const { readFileSync } = require('fs');
+
+// get all files
+const homePage = readFileSync('./index.html');
 
 const server = http.createServer((req, res) => {
   // console.log(req.method);
@@ -9,7 +13,7 @@ const server = http.createServer((req, res) => {
   // home page
   if (url === '/') {
     res.writeHead(200, { 'content-type': 'text/html' });
-    res.write('<h1>home page</h1>');
+    res.write(homePage);
     res.end();
   }
   // about page
