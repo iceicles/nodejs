c0c218138626a66d4ee7ce90f4c64625c43ae7eb
oshiogifted <oshiogifted2011@gmail.com>
2024-10-09 22:43:10 -0400

    sending html file by adding it to the static public folder

---
+++ 2-express-tutorial/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// setup static (assets/files that server don't need to change) and middleware (app.use)
+// note: browser-app.js is part of /public because even tho JS is dynamic in nature, it is dynamic on the browser (in this case)
+app.use(express.static('./public'));
+
+// app.get('/', (req, res) => {
+//   res.sendFile(path.resolve(__dirname, './navbar-app/index.html'));
+// two ways to sending html files -
+// adding to static assets
+// SSR using template engine
+// });
+
+app.all('*', (req, res) => {
+  res.status(404).send('resource not found');
+});
+
+app.listen(5000, () => {
+  console.log(`server is listening on port 5000...`);
+});
