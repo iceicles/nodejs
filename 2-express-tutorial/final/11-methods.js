0e265d9bbd3035072911e9c1cc05198ee6e6afd4
oshiogifted <oshiogifted2011@gmail.com>
2024-10-12 22:29:48 -0400

    post method using javascript.html file in methods-public

--- 2-express-tutorial/final/11-methods.js
+++ 2-express-tutorial/final/11-methods.js
@@ -9,8 +9,22 @@ app.use(express.static('./methods-public'));
 // urlencoded parses incoming reqs and populates the request's body (req.body) by creating a new object containing the parsed data
 app.use(express.urlencoded({ extended: false }));
 
+// parse json data
+// middleware that only parses json
+app.use(express.json());
+
 app.get('/api/people', (req, res) => {
   res.status(200).json({ success: true, data: people });
+});
+
+app.post('/api/people', (req, res) => {
+  const { name } = req.body; // can do this thanks to the middleware express.json that parses and attaches to body
+  console.log('name - ', name);
+  if (!name)
+    return res
+      .status(400)
+      .json({ sucess: false, msg: 'please provide name value' });
+  res.status(201).json({ success: true, person: name });
 });
 
 app.post('/login', (req, res) => {
