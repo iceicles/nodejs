67d4b74b387c0d78b8b278b15639f9f482ee89c6
oshiogifted <oshiogifted2011@gmail.com>
2024-10-13 14:00:09 -0400

    put method

--- 2-express-tutorial/final/11-methods.js
+++ 2-express-tutorial/final/11-methods.js
@@ -49,6 +49,27 @@ app.post('/login', (req, res) => {
   //res.send('POST');
 });
 
+app.put('/api/people/:id', (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+
+  // console.log(id, name);
+  const person = people.find((person) => person.id === Number(id));
+
+  if (!person) {
+    return res
+      .status(404)
+      .json({ success: false, msg: `no person with id ${id}` });
+  }
+
+  const newPeople = people.map((person) => {
+    if (person.id === Number(id)) {
+      person.name = name;
+    }
+    return person;
+  });
+  res.status(200).json({ success: true, data: newPeople });
+});
 app.listen(5000, () => {
   console.log('Server is listening on port 5000...');
 });
