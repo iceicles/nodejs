d917b2d7f6f58262bba97a92a83875350d01867a
oshiogifted <oshiogifted2011@gmail.com>
2024-10-20 22:27:17 -0400

    creating tasks collection with post req body sent to mongodb

--- 3-task-manager/final/models/Task.js
+++ 3-task-manager/final/models/Task.js
@@ -5,4 +5,6 @@ const TaskSchema = new mongoose.Schema({
   completed: Boolean,
 });
 
+// collection name = Task (note: mongoose saves this as plural lowercase, hence it'll be tasks)
+// schema (aka data model) = TaskSchema
 module.exports = mongoose.model('Task', TaskSchema);
