0b27bfa0667fd9d41eb26b1115723eaa1ebc26d7
oshiogifted <oshiogifted2011@gmail.com>
2024-10-22 18:09:07 -0400

    delete task using mongoose findOneAndDelete query

--- 3-task-manager/final/controllers/tasks.js
+++ 3-task-manager/final/controllers/tasks.js
@@ -40,8 +40,22 @@ const updateTask = (req, res) => {
   res.send('update task');
 };
 
-const deleteTask = (req, res) => {
-  res.send('delete task');
+const deleteTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOneAndDelete({ _id: taskID });
+
+    if (!task) {
+      return res.status(404).json({ msg: `No task with id : ${taskID}` });
+    }
+
+    res.status(200).json({ task });
+    // don't need to send a response when you're deleting something -
+    // res.status(200).send();
+    // res.status(200).json({task: null, status: 'success'});
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
 module.exports = {
