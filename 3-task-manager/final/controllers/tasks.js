bec466d7c6f5d5080295e872e339aebf3949ae23
oshiogifted <oshiogifted2011@gmail.com>
2024-10-24 18:07:14 -0400

    async wrapper with try/catch block

--- 3-task-manager/final/controllers/tasks.js
+++ 3-task-manager/final/controllers/tasks.js
@@ -1,76 +1,53 @@
 const Task = require('../models/Task');
+const asyncWrapper = require('../middleware/async');
 
-const getAllTasks = async (req, res) => {
-  try {
-    const tasks = await Task.find({});
-    res.status(200).json({ tasks });
-  } catch (error) {
-    res.status(500).json({ msg: error });
+const getAllTasks = asyncWrapper(async (req, res) => {
+  const tasks = await Task.find({});
+  res.status(200).json({ tasks });
+});
+
+const createTask = asyncWrapper(async (req, res) => {
+  const task = await Task.create(req.body);
+  res.status(201).json({ task });
+});
+
+const getTask = asyncWrapper(async (req, res) => {
+  const { id: taskID } = req.params;
+  const task = await Task.findOne({ _id: taskID }); // or findById(taskId)
+  if (!task) {
+    // if task ID is right length but doesn't exist
+    return res.status(404).json({ msg: `No task with id : ${taskID}` });
   }
-};
+  res.status(200).send({ task });
+});
 
-const createTask = async (req, res) => {
-  // wrapping in try/catch block to gracefully handle invalid body requests as setup and validated in our schema
-  try {
-    // remember - req.body is the body attached to the request...
-    // ...and 'app.use(express.json())' middleware parses the body of the request of type json and attaches it to req property
-    const task = await Task.create(req.body);
-    res.status(201).json({ task });
-  } catch (error) {
-    res.status(500).json({ msg: error });
+const updateTask = asyncWrapper(async (req, res) => {
+  const { id: taskID } = req.params;
+  const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
+    new: true, // get back new item (without it, it'll respond the old one)
+    runValidators: true, // make sure it follows our schema validation
+  });
+
+  if (!task) {
+    return res.status(404).json({ msg: `No task with id : ${taskID}` });
   }
-};
 
-const getTask = async (req, res) => {
-  try {
-    const { id: taskID } = req.params;
-    const task = await Task.findOne({ _id: taskID }); // or findById(taskId)
-    if (!task) {
-      // if task ID is right length but doesn't exist
-      return res.status(404).json({ msg: `No task with id : ${taskID}` });
-    }
-    res.status(200).send({ task });
-  } catch (error) {
-    // if taskID is wrong length (responds with mongoose err msg)
-    res.status(500).json({ msg: error });
+  res.status(200).json({ task });
+});
+
+const deleteTask = asyncWrapper(async (req, res) => {
+  const { id: taskID } = req.params;
+  const task = await Task.findOneAndDelete({ _id: taskID });
+
+  if (!task) {
+    return res.status(404).json({ msg: `No task with id : ${taskID}` });
   }
-};
 
-const updateTask = async (req, res) => {
-  try {
-    const { id: taskID } = req.params;
-    const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
-      new: true, // get back new item (without it, it'll respond the old one)
-      runValidators: true, // make sure it follows our schema validation
-    });
-
-    if (!task) {
-      return res.status(404).json({ msg: `No task with id : ${taskID}` });
-    }
-
-    res.status(200).json({ task });
-  } catch (error) {
-    res.status(500).json({ msg: error });
-  }
-};
-
-const deleteTask = async (req, res) => {
-  try {
-    const { id: taskID } = req.params;
-    const task = await Task.findOneAndDelete({ _id: taskID });
-
-    if (!task) {
-      return res.status(404).json({ msg: `No task with id : ${taskID}` });
-    }
-
-    res.status(200).json({ task });
-    // don't need to send a response when you're deleting something -
-    // res.status(200).send();
-    // res.status(200).json({task: null, status: 'success'});
-  } catch (error) {
-    res.status(500).json({ msg: error });
-  }
-};
+  res.status(200).json({ task });
+  // don't need to send a response when you're deleting something -
+  // res.status(200).send();
+  // res.status(200).json({task: null, status: 'success'});
+});
 
 module.exports = {
   getAllTasks,
---
+++ 3-task-manager/final/middleware/async.js
@@ -0,0 +1,12 @@
+// wraps the function in our controller with try/catch block
+const asyncWrapper = (fn) => {
+  return async (req, res, next) => {
+    try {
+      await fn(req, res, next);
+    } catch (error) {
+      next(error);
+    }
+  };
+};
+
+module.exports = asyncWrapper;
