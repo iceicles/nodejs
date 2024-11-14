67f95672e2e2a10118a97190dd64618e028f0b4a
oshiogifted <oshiogifted2011@gmail.com>
2024-11-14 17:42:07 -0500

    using express async errors to throw erros (no need to call next)

--- 4-store-api/final/controllers/products.js
+++ 4-store-api/final/controllers/products.js
@@ -1,4 +1,7 @@
 const getAllProductsStatic = async (req, res) => {
+  // we can use the pkg 'express-async-errors' to throw errors for routes.
+  // no need to call next() function provided by express to call middleware with err msgs
+  throw new Error('testing async errors');
   res.status(200).json({ msg: 'products testing route' });
 };
 
