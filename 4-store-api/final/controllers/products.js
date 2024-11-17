45fd0272061833ac485f98e737c2eef684bf742d
oshiogifted <oshiogifted2011@gmail.com>
2024-11-16 22:54:50 -0500

    basic find

--- 4-store-api/final/controllers/products.js
+++ 4-store-api/final/controllers/products.js
@@ -1,8 +1,13 @@
+const Product = require('../models/product');
+
 const getAllProductsStatic = async (req, res) => {
+  const products = await Product.find({
+    name: 'vase table',
+  });
   // we can use the pkg 'express-async-errors' to throw errors for routes.
   // no need to call next() function provided by express to call middleware with err msgs
-  throw new Error('testing async errors');
-  res.status(200).json({ msg: 'products testing route' });
+  // throw new Error('testing async errors');
+  res.status(200).json({ products, nbHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
