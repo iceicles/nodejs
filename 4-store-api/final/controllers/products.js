7ce9927bc69231204672a2b9974dba7490d6f3b2
oshiogifted <oshiogifted2011@gmail.com>
2024-11-24 18:03:53 -0500

    pagination

--- 4-store-api/final/controllers/products.js
+++ 4-store-api/final/controllers/products.js
@@ -41,6 +41,14 @@ const getAllProducts = async (req, res) => {
     console.log(fields);
   }
 
+  // skip and limit for pagination
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10; // default to 10 products if no limit is passed
+  // ex., for page = 1 => 1 - 1 * 10 = 0 => page 1 | for page = 2 => 2 - 1 * 10 = 10 => page 2 bc skip 10 will skip the first 10
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
+
   const products = await result;
   res.status(200).json({ products, nbHits: products.length });
 };
