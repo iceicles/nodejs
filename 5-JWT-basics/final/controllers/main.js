60b9eb42857b0a10238b48616685c9f382dc470e
oshiogifted <oshiogifted2011@gmail.com>
2024-11-25 17:26:04 -0500

    validate username and password

--- 5-JWT-basics/final/controllers/main.js
+++ 5-JWT-basics/final/controllers/main.js
@@ -1,4 +1,19 @@
+/* 
+- check username, password in post(login) request
+- if it exists, create new JWT on server and send to client
+- setup authentication so only the request with JWT can access the dashboard
+*/
+
+const CustomAPIError = require('../errors/custom-error');
+
 const login = async (req, res) => {
+  const { username, password } = req.body;
+  // three ways to verify user/pass - mongo required(in schema) validations, Joi pkg, check in controller
+  if (!username || !password) {
+    throw new CustomAPIError('Please provide email and password', 400);
+  }
+
+  console.log(username, password);
   res.send('Fake Login/Register/Signup Route');
 };
 
