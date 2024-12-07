dfe21514597ddb5c41516e478b25d8c3c9c9951d
oshiogifted <oshiogifted2011@gmail.com>
2024-12-07 17:49:03 -0500

    login controller setup

--- 6-jobs-api/controllers/auth.js
+++ 6-jobs-api/controllers/auth.js
@@ -1,5 +1,6 @@
 const User = require('../models/User');
 const { StatusCodes } = require('http-status-codes');
+const { BadRequestError, UnauthenticatedError } = require('../errors');
 
 const register = async (req, res) => {
   const user = await User.create({ ...req.body });
@@ -8,7 +9,24 @@ const register = async (req, res) => {
 };
 
 const login = async (req, res) => {
-  res.send('login user');
+  const { email, password } = req.body;
+
+  // if no email or password is initially provided by user
+  if (!email || !password) {
+    throw new BadRequestError('Please provide email and password');
+  }
+
+  // find the email (as inputed by the user in request body) in the database
+  const user = await User.findOne({ email });
+
+  // if user email doesn't exist in database
+  if (!user) {
+    throw new UnauthenticatedError('Invalid Credentials');
+  }
+
+  // create token
+  const token = user.createJWT(); // get back token
+  res.status(StatusCodes.OK).json({ user: { name: user.name }, token });
 };
 
 module.exports = {
