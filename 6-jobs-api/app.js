a9a0a5609456c344088d2c116b99b1897c3dce38
oshiogifted <oshiogifted2011@gmail.com>
2024-12-13 15:11:40 -0500

    security setup

--- 6-jobs-api/app.js
+++ 6-jobs-api/app.js
@@ -1,5 +1,12 @@
 require('dotenv').config();
 require('express-async-errors');
+
+// extra security packages
+const helmet = require('helmet');
+const cors = require('cors');
+const xss = require('xss-clean');
+const rateLimiter = require('express-rate-limit');
+
 const express = require('express');
 const app = express();
 
@@ -18,7 +25,19 @@ const errorHandlerMiddleware = require('./middleware/error-handler');
 const auth = require('./middleware/authentication');
 
 // extra packages
+app.set('trust proxy', 1 /* number of proxies between user and server */);
+app.use(
+  rateLimiter({
+    // how long
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    // how many requests
+    limit: 100, // limit each IP to 100 requests per `window` (here, per 15 minutes).
+  })
+);
 app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
 // routes
 app.use('/api/v1/auth', authRouter);
