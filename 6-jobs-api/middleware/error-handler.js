933aec7a06e63b3ad1469602acafc3e6b920a102
oshiogifted <oshiogifted2011@gmail.com>
2024-12-13 16:46:50 -0500

    removed console log

--- 6-jobs-api/middleware/error-handler.js
+++ 6-jobs-api/middleware/error-handler.js
@@ -39,7 +39,6 @@ const errorHandlerMiddleware = (err, req, res, next) => {
     ``;
     customError.statusCode = 400;
   }
-  console.log('or here?');
   return res.status(customError.statusCode).json({ msg: customError.msg });
   // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err });
 };
