ce4646dd4bda2d6d8ccf372b7c0dd782b65aea3a
oshiogifted <oshiogifted2011@gmail.com>
2024-12-30 18:37:32 -0500

    auth routes struct

--- 10-e-commerce-api/app.js
+++ 10-e-commerce-api/app.js
@@ -10,6 +10,9 @@ const morgan = require('morgan');
 // database
 const connectDB = require('./db/connect');
 
+// routers
+const authRouter = require('./routes/auth');
+
 // middleware
 const notFoundMW = require('./middleware/not-found');
 const errorHandlerMW = require('./middleware/error-handler');
@@ -17,9 +20,12 @@ const errorHandlerMW = require('./middleware/error-handler');
 app.use(morgan('tiny'));
 app.use(express.json());
 
+// routes
 app.get('/', (req, res) => {
   res.send('<h1>E-commerce API</h1>');
 });
+
+app.use('/api/v1/auth', authRouter);
 
 app.use(notFoundMW);
 app.use(errorHandlerMW);
