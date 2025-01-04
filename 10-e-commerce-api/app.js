fd44864594b2f4cbddc9ee736a5aac5feb70b9ce
oshiogifted <oshiogifted2011@gmail.com>
2025-01-03 19:33:52 -0500

    user routes structure

--- 10-e-commerce-api/app.js
+++ 10-e-commerce-api/app.js
@@ -13,6 +13,7 @@ const connectDB = require('./db/connect');
 
 // routers
 const authRouter = require('./routes/auth');
+const userRouter = require('./routes/user');
 
 // middleware
 const notFoundMW = require('./middleware/not-found');
@@ -34,6 +35,7 @@ app.get('/api/v1', (req, res) => {
 });
 
 app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/users', userRouter);
 
 app.use(notFoundMW);
 app.use(errorHandlerMW);
---
+++ 10-e-commerce-api/routes/user.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+const {
+  getAllUsers,
+  getSingleUser,
+  showCurrentUser,
+  updateUser,
+  updateUserPassword,
+} = require('../controllers/user');
+
+const router = express.Router();
+
+router.route('/').get(getAllUsers);
+
+// the next three routes are specifically placed above the route with /:id bc /showMe (or /updateUser or /updateUserPassword) will be treated as path param & will throw an err
+// a workaround will be to have something like /user(or admin) /showMe (or updateUser, updateUserPassword)
+router.route('/showMe').get(showCurrentUser);
+router.route('/updateUser').post(updateUser);
+router.route('/updateUserPassword').post(updateUserPassword);
+
+router.route('/:id').get(getSingleUser);
+
+module.exports = router;
