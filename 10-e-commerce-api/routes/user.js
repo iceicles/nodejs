76c4f9ae7df9949567f6872ab7fc5a178fd5b659
oshiogifted <oshiogifted2011@gmail.com>
2025-01-04 17:20:58 -0500

    using patch instead

--- 10-e-commerce-api/routes/user.js
+++ 10-e-commerce-api/routes/user.js
@@ -15,8 +15,8 @@ router.route('/').get(getAllUsers);
 // the next three routes are specifically placed above the route with /:id bc /showMe (or /updateUser or /updateUserPassword) will be treated as path param & will throw an err
 // a workaround will be to have something like /user(or admin) /showMe (or updateUser, updateUserPassword)
 router.route('/showMe').get(showCurrentUser);
-router.route('/updateUser').post(updateUser);
-router.route('/updateUserPassword').post(updateUserPassword);
+router.route('/updateUser').patch(updateUser);
+router.route('/updateUserPassword').patch(updateUserPassword);
 
 router.route('/:id').get(getSingleUser);
 
