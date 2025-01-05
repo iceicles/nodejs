2b90afeff76b6899af52ac7910799b2c79273d23
oshiogifted <oshiogifted2011@gmail.com>
2025-01-05 14:55:24 -0500

    added createTokenUser to utils

--- 10-e-commerce-api/controllers/auth.js
+++ 10-e-commerce-api/controllers/auth.js
@@ -1,7 +1,7 @@
 const User = require('../models/User');
 const { StatusCodes } = require('http-status-codes');
 const CustomError = require('../errors');
-const { attachCookiesToResponse } = require('../utils');
+const { attachCookiesToResponse, createTokenUser } = require('../utils');
 
 const register = async (req, res) => {
   const { name, password, email } = req.body;
@@ -16,7 +16,7 @@ const register = async (req, res) => {
 
   const user = await User.create({ name, email, password, role });
 
-  const tokenUser = { name: user.name, userId: user._id, role: user.role };
+  const tokenUser = createTokenUser(user);
 
   attachCookiesToResponse({ res, user: tokenUser });
 
@@ -45,7 +45,7 @@ const login = async (req, res) => {
   }
 
   // attach cookie to response
-  const tokenUser = { name: user.name, userId: user._id, role: user.role };
+  const tokenUser = createTokenUser(user);
   attachCookiesToResponse({ res, user: tokenUser });
   res.status(StatusCodes.CREATED).json({ user: tokenUser });
 };
