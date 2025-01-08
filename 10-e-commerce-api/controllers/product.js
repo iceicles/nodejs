8e49272972c2fd1a8daf3094fdf04782561659a9
oshiogifted <oshiogifted2011@gmail.com>
2025-01-08 17:28:25 -0500

    createProduct controller

--- 10-e-commerce-api/controllers/product.js
+++ 10-e-commerce-api/controllers/product.js
@@ -1,5 +1,21 @@
+const { StatusCodes } = require('http-status-codes');
+const Product = require('../models/Product');
+const CustomError = require('../errors');
+
 const createProduct = async (req, res) => {
-  res.send('create product');
+  // using userId to know which user created which product
+  // remember: any request made by the client will always include the cookie which has the access JWT
+  const { userId } = req.user;
+
+  // pass the user to req.body
+  // note that 'user' property needs to match what is setup in ProductSchema
+  req.body.user = userId;
+
+  // then create the product with the product inputed by the user
+  // which now also includes user property with userId
+  const product = await Product.create(req.body);
+
+  res.status(StatusCodes.CREATED).json({ product });
 };
 const getAllProducts = async (req, res) => {
   res.send('get all products');
