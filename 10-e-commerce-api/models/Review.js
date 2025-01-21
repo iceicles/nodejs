d9fa537e0cf579eb1e524a22baba0ef049bf6027
oshiogifted <oshiogifted2011@gmail.com>
2025-01-20 22:37:52 -0500

    review model

---
+++ 10-e-commerce-api/models/Review.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+
+const ReviewModel = new mongoose.Schema(
+  {
+    rating: {
+      type: Number,
+      min: 1,
+      max: 5,
+      required: [true, 'Please provide rating'],
+    },
+    title: {
+      type: String,
+      trim: true,
+      required: [true, 'Please provide review title'],
+      maxlength: 100,
+    },
+    comment: {
+      type: String,
+      required: [true, 'Please provdie review text'],
+    },
+    user: {
+      // every review will be tied to a user
+      type: mongoose.Types.ObjectId,
+      ref: 'User', // user model
+      required: true,
+    },
+    product: {
+      // every review ill be tied to a product
+
+      type: mongoose.Types.ObjectId,
+      ref: 'Product', // product model
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// ensure the user can leave only one review per product
+ReviewSchema.index({ product: 1, user: 1 }, { unique: true });
+
+module.exports = mongoose.model('Review', ReviewSchema);
