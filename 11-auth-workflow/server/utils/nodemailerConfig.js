76891c0650098fc80fc067189d55d7e862959a37
oshiogifted <oshiogifted2011@gmail.com>
2025-01-26 19:53:01 -0500

    send email functionality

---
+++ 11-auth-workflow/server/utils/nodemailerConfig.js
@@ -0,0 +1,8 @@
+module.exports = {
+  host: 'smtp.ethereal.email',
+  port: 587,
+  auth: {
+    user: 'forrest.blanda62@ethereal.email',
+    pass: 'xwCgNuR6tpXzxfA6EQ',
+  },
+};
--- 11-auth-workflow/server/utils/sendEmail.js
+++ 11-auth-workflow/server/utils/sendEmail.js
@@ -1,22 +1,15 @@
 const nodemailer = require('nodemailer');
+const nodemailerConfig = require('./nodemailerConfig');
 
-const sendEmail = async () => {
-  const transporter = nodemailer.createTransport({
-    host: 'smtp.ethereal.email',
-    port: 587,
-    auth: {
-      user: 'forrest.blanda62@ethereal.email',
-      pass: 'xwCgNuR6tpXzxfA6EQ',
-    },
-  });
+const sendEmail = async ({ to, subject, html }) => {
+  const transporter = nodemailer.createTransport(nodemailerConfig);
 
   // send mail with defined transport object
-  const info = await transporter.sendMail({
+  return transporter.sendMail({
     from: '"Iceicles" <iceicles@gmail.com>', // sender address
-    to: 'user@user.com', // list of receivers
-    subject: 'Testing email ✔', // Subject line
-    text: 'Hello world?', // plain text body
-    html: '<b>Testing email?</b>', // html body
+    to,
+    subject,
+    html,
   });
 };
 
