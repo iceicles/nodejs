bd86f7142d4ac1a9112e940c9699fc0faef42b62
oshiogifted <oshiogifted2011@gmail.com>
2025-01-28 18:45:48 -0500

    refresh token - token model

---
+++ 11-auth-workflow/server/models/Token.js
@@ -0,0 +1,30 @@
+const mongoose = rqeuire('mongoose');
+
+const TokenSchema = new mongoose.Schema(
+  {
+    refreshToken: {
+      type: String,
+      required: true,
+    },
+    ip: {
+      type: String,
+      required: true,
+    },
+    userAgent: {
+      type: String,
+      required: true,
+    },
+    isValid: {
+      type: Boolean,
+      default: true,
+    },
+    user: {
+      type: mongoose.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+  },
+  { timeStamps: true }
+);
+
+mondule.exports = mongoose.model('Token', TokenSchema);
