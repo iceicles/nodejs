1f84b0d1dfaee60f2061de933a6700bb77c6315f
oshiogifted <oshiogifted2011@gmail.com>
2025-01-28 19:00:42 -0500

    refresh token - create token in login controller

--- 11-auth-workflow/server/controllers/authController.js
+++ 11-auth-workflow/server/controllers/authController.js
@@ -92,9 +92,22 @@ const login = async (req, res) => {
   }
 
   const tokenUser = createTokenUser(user);
-  attachCookiesToResponse({ res, user: tokenUser });
 
-  res.status(StatusCodes.OK).json({ user: tokenUser });
+  // create refresh token
+  let refreshToken = '';
+  // check for existing token
+
+  refreshToken = crypto.randomBytes(40).toString('hex');
+  const userAgent = req.headers['user-agent'];
+  const ip = req.ip;
+  const userToken = { refreshToken, ip, userAgent, user: user._id };
+
+  // create token
+  const token = await Token.create(userToken);
+
+  // attachCookiesToResponse({ res, user: tokenUser });
+
+  res.status(StatusCodes.OK).json({ user: tokenUser, token });
 };
 
 const verifyEmail = async (req, res) => {
