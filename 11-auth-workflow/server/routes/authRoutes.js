6ebdc4c381f570d880c3a292d06d6cace0bc6c80
oshiogifted <oshiogifted2011@gmail.com>
2025-02-02 19:48:07 -0500

    fix logout functionality

--- 11-auth-workflow/server/routes/authRoutes.js
+++ 11-auth-workflow/server/routes/authRoutes.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+const { authenticateUser } = require('../middleware/authentication');
+
 const {
   register,
   login,
@@ -10,7 +12,9 @@ const {
 
 router.post('/register', register);
 router.post('/login', login);
-router.get('/logout', logout);
+// the reason we add the authenticateUser middleware before logout controller is..
+// ..we need to get req.user to remove the token from the db and auth MW attaches it
+router.delete('/logout', authenticateUser, logout);
 router.post('/verify-email', verifyEmail);
 
 module.exports = router;
